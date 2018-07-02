d5b64e64fa69a5aa249f26384332c0db3c951b0f
Flavins <maduabuchiokonkwo@gmail.com>
2018-07-02 23:56:59 +0100

    add popup window for marker feature

--- map1.py
+++ map1.py
@@ -4,13 +4,14 @@ import pandas
 data = pandas.read_csv("Volcanoes_USA.txt")
 lat = list(data["LAT"])
 lon = list(data["LON"])
+elev = list(data["ELEV"])
 map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Mapbox Bright")
 
 fg = folium.FeatureGroup(name="My Map")
 
-for lt, ln in zip(lat, lon):
+for lt, ln, el in zip(lat, lon, elev):
 
-    fg.add_child(folium.Marker(location=[lt, ln], popup="Hi welcome", icon=folium.Icon(color="red")))
+    fg.add_child(folium.Marker(location=[lt, ln], popup=str(el)+" m", icon=folium.Icon(color="red")))
 map.add_child(fg)
 
 map.save("map1.html")
