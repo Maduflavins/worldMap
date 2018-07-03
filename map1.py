0e4a9de781b333c72647396bbd6e2ffcb4134368
Flavins <maduabuchiokonkwo@gmail.com>
2018-07-03 19:33:13 +0100

    change the icon marker to circlemarker

--- map1.py
+++ map1.py
@@ -20,7 +20,8 @@ fg = folium.FeatureGroup(name="My Map")
 
 for lt, ln, el in zip(lat, lon, elev):
 
-    fg.add_child(folium.Marker(location=[lt, ln], popup=str(el)+" m", icon=folium.Icon(color_producer(el))))
+    fg.add_child(folium.CircleMarker(location=[lt, ln], radius = 6,  popup=str(el)+" m",
+    fill_color=color_producer(el), color='grey', fill=True, fill_opacity=0.7))
 map.add_child(fg)
 
 map.save("map1.html")
